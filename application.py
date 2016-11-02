c7eaf95f33314bd6e7b5a6aba17b2a519a89c826
dmattia <dmattia@nd.edu>
2016-11-02 17:20:02 -0400

    Added yelp api

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, request
 from crossdomain import crossdomain
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+import os
+import json
 import requests
 
 application = Flask(__name__)
@@ -17,6 +22,33 @@ def typeform_get():
   r = requests.get(url)
   context = r.json()["responses"][0]["answers"]
   return jsonify(**context)
+
+@application.route("/yelp", methods=["GET"])
+def yelp_search():
+  # read API keys
+  if "consumer_key" in os.environ:
+    auth = Oauth1Authenticator(
+      consumer_key=os.environ['consumer_key'],
+      consumer_secret=os.environ['consumer_secret'],
+      token=os.environ['token'],
+      token_secret=os.environ['token_secret']
+    )
+  else:
+    with open('config_secret.json') as cred:
+      creds = json.load(cred)
+      auth = Oauth1Authenticator(**creds)
+      client = Client(auth)
+
+  client = Client(auth)
+
+  params = {
+    'term': 'food',
+    'lang': 'en'
+  }
+  response = client.search('South Bend, IN', **params)
+  businesses = response.businesses
+  results = {b.name: {"business_url": b.url, "image_url": b.image_url, "snippet": b.snippet_text} for b in businesses}
+  return jsonify(**results)
 
 if __name__ == "__main__":
   application.debug = True
